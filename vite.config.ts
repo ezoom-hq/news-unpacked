98cb6839018a3b3d7fa38d16968d9cd85252957b
ezoom <ezoom.hq@gmail.com>
2025-12-19 23:44:49 +0900

    修正 リポジトリ名決定

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: '/news-unpacked/', // TODO: GitHubのリポジトリ名に合わせて変更してください
-})
+    base: mode === 'production' ? '/news-unpacked/' : '/',
+}))
